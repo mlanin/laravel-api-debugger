4d37e5932ce311c61142ea3860502efdcf4ee243
Maxim Lanin <maxim.lanin@acronis.com>
2016-10-18 12:29:04 +0300

    Add lad helper

---
+++ src/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+use Lanin\ApiDebugger\Debugger;
+
+if (! function_exists('lad')) {
+    /**
+     * Dump the passed variables.
+     *
+     * @param  mixed
+     * @return void
+     */
+    function lad()
+    {
+        call_user_func_array([app(Debugger::class), 'dump'], func_get_args());
+    }
+}
