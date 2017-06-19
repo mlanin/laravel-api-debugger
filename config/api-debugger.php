31aa3656b07cb2c1898cdfe92b5de129f87aba08
Maxim Lanin <max@lanin.me>
2017-06-19 16:05:49 +0300

    Sort collections

--- config/api-debugger.php
+++ config/api-debugger.php
@@ -8,10 +8,10 @@ return [
 		// Database queries.
 		\Lanin\Laravel\ApiDebugger\Collections\QueriesCollection::class,
 
-		// Profile custom events.
-		\Lanin\Laravel\ApiDebugger\Collections\ProfilingCollection::class,
-
 		// Show cache events.
 		\Lanin\Laravel\ApiDebugger\Collections\CacheCollection::class,
+
+		// Profile custom events.
+		\Lanin\Laravel\ApiDebugger\Collections\ProfilingCollection::class,
 	],
 ];
