de73e5c39bc639176d406ffbd8daef27352d80b2
Maksim Lanin <mslanin@avito.ru>
2019-08-06 12:37:27 +0300

    Add more tests

--- tests/DebuggerTest.php
+++ tests/DebuggerTest.php
@@ -201,7 +201,7 @@ class DebuggerTest extends TestCase
     }
 
     /** @test */
-    public function it_preserves_structure()
+    public function it_preserves_object()
     {
         $this->app['router']->get('foo', function () {
             return response()->json([
@@ -213,5 +213,20 @@ class DebuggerTest extends TestCase
         $this->json('get', '/foo')
             ->assertStatus(200)
             ->assertSeeText('"baz":{}');
+    }
+
+    /** @test */
+    public function it_preserves_array()
+    {
+        $this->app['router']->get('foo', function () {
+            return response()->json([
+                'foo' => 'bar',
+                'baz' => [],
+            ]);
+        });
+
+        $this->json('get', '/foo')
+            ->assertStatus(200)
+            ->assertSeeText('"baz":[]');
     }
 }
